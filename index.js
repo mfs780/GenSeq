513101f4a401e73e796e95f025faa01dd03d7a59
Fahad Sheikh <fahadsheikh780@gmail.com>
2016-12-06 19:10:01 -0800

    near final

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ var ruleSet;
 var outputArray;
 var lineLength;
 
-var sfinal = '1 2 A 3 4 2 5 6 7 8 9 10 11 12 13 14 15 16 11 17 18 19 20 21 22 23 13 24 25 14 26 27 28 29 30 31 32 33 25 34 27 3 21 6 35 36 6 37 12 27 38 10 39 40 1 41 42 21 43 36 11 6 42 44 45 39 29 4 46 27 47 41 31 48 1 49 36 50 13 21 51 36 17 52 53 54 55 5 43 12 35 20 56 23 46 36 57 51 56 21 47 9 52 17 25 58 59 46 30 60 46 55 46 59 10 61 36 62 34 63 64 65 31 10 58 22 43 T 48 55 T 66 18 T 67 68 69 66 T 64 70 55 71 50 72 67 49 69 16 21 40 63 70 58 73 74 74 49 75 T 76 61 46 29 73 61 77 78 G 79 32 6 48 T 57 T 46 78 29 21 3 80 69 45 20 29 63 81 51 29 12 82 59 51 58 79 54 33 38 83 71 84 41 83 46 65 61 84 16 85 61 12 65 49 10 86 48 58 71 68 66 50 87 49 60 61 62 84 T 27 88 15 32 89 79 29 87 34 46 76 T 60 41 90 81 75 91 77 2 0 83 89 70 82 86 6 26 16 50 71 92 36 80 89 T 91 90 1 46 88 80 23 41 49 85 T 21 19 34 37 88 T 89 43 41 23 83 28 72 70 23 34 17 23 70 46 46 80 53 48 35 T 84 46 44 G A';
 program
   .version('0.0.1')
   .description('A DNA Compression Program')
@@ -31,14 +30,13 @@ program
 
 // console.log(dna);
 // console.log(start(dna));
-
 if (program.input && program.output) {
   if (program.algorithm == "huffman") {
     let inputData = fs.readFileSync(program.input);
     let wstream, outputData;
 
     if (program.type == "e") {
-      console.log('Huffman Encoding', program.input);
+      console.log('Huffman Encoding', program.input, inputData);
       wstream = fs.createWriteStream(program.output + "_compressed");
       outputData = compressjs.Huffman.compressFile(inputData);
     } else if (program.type == "d") {
@@ -50,33 +48,42 @@ if (program.input && program.output) {
     wstream.write(new Buffer(outputData));
     wstream.end();
   } else if (program.algorithm == "sequitur") {
-              
+
     let wstream, wcstream, codeArray, codeData, inputData, outputArray, outputData;
+    let c_data, c_code;
 
     if (program.type == "e" && program.code) {
       console.log('Sequitur Compressing using Code:' + program.code + ' on Input:' + program.input);
       wstream = fs.createWriteStream(program.output + '_' + program.code + '_compressed');
       inputData = fs.readFileSync(program.input).toString();
-      codeData = fs.readFileSync(program.code).toString();
+      codeData = fs.readFileSync(program.code);
+      codeData = new Buffer(compressjs.Huffman.decompressFile(codeData)).toString();
       outputData = encode(codeData, inputData) + ' ';
-      wstream.write(new Buffer(outputData));
+      
+      wstream.write(new Buffer(compressjs.Huffman.compressFile(new Buffer(outputData))));
       wstream.end();
     } else if (program.type == "e") {
+      console.log('Sequitur Compressing on Input:' + program.input);
       wstream = fs.createWriteStream(program.output + '_data');
       wcstream = fs.createWriteStream(program.output + '_code');
       inputData = fs.readFileSync(program.input);
       outputArray = start(inputData.toString());
-      wstream.write(new Buffer(outputArray.shift()));
+
+      wstream.write(new Buffer(compressjs.Huffman.compressFile(new Buffer(outputArray.shift()))));
       wstream.end();
 
-      wcstream.write(new Buffer(outputArray.join('\n')));
+      wcstream.write(new Buffer(compressjs.Huffman.compressFile(new Buffer(outputArray.join('\n')))));
       wcstream.end();
-      
+
     } else if (program.type == "d" && program.code) {
       console.log('Sequitur Decoding using Code:' + program.code + ' on Input:' + program.input);
       wstream = fs.createWriteStream(program.output + '_decompressed');
       inputData = fs.readFileSync(program.input);
-      codeData = fs.readFileSync(program.code).toString().split('\n');
+      inputData = new Buffer(compressjs.Huffman.decompressFile(inputData)).toString();
+      console.log(inputData);
+
+      codeData = fs.readFileSync(program.code);
+      codeData = new Buffer(compressjs.Huffman.decompressFile(codeData)).toString().split('\n');
       outputData = decode(codeData, ' ' + inputData);
       wstream.write(new Buffer(outputData));
       wstream.end();
@@ -84,7 +91,10 @@ if (program.input && program.output) {
   }
 }
 
+
+
 // Helper
+
 function encode(code, data) {
   code = code.split('\n');
   data = data.split('').join(' ');
@@ -95,7 +105,7 @@ function encode(code, data) {
     hasReplace = false;
     for (let i = 0; i < code.length; i++) {
       hasReplace = false;
-      if(code[i].length){
+      if (code[i].length) {
         // console.log(i + 1, code[i]);
         hasReplace = !!data.indexOf(code[i]);
         data = data.replace(new RegExp(code[i], 'g'), (i + 1) + ' ');
